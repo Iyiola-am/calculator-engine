b70bcd8cbe514d2175dc62eaf7b7780c70079185
Iyiola-am <adekunle3317@gmail.com>
2021-01-27 03:45:55 +0100

    Refactored evaluators

--- evaluators.ts
+++ evaluators.ts
@@ -1,6 +1,7 @@
-export default {
+let evaluators: {[key: string]: Function} = {
     '/': (a: number, b: number) => (a / b),
     '*': (a: number, b: number) => (a * b),
     '+': (a: number, b: number) => (a + b),
     '-': (a: number, b: number) => (a - b),
-}
+};
+export default evaluators;
